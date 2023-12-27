2b791a564ed021b2dd4faf3342065c85bb725efc
Mauricio Monzon <104470486+Majomon@users.noreply.github.com>
2023-12-27 13:14:19 -0300

    Img

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,15 +1,21 @@
 "use client";
-import { Image } from "@nextui-org/react";
-import Newlabel from "../Newlabel/Newlabel";
 import Link from "next/link";
+import Newlabel from "../Newlabel/Newlabel";
 
-export default function Card({ images, title, price, cantDues, newProduct,id }) {
+export default function Card({
+  images,
+  title,
+  price,
+  cantDues,
+  newProduct,
+  id,
+}) {
   return (
     <div className=" w-[240px] h-[350px] rounded-md shadow-3xl">
       <section className="w-[100%]  h-[250px] relative">
         <Newlabel newProduct={newProduct} />
         <Link href={`/detail/${id}`}>
-          <Image
+          <img
             className="w-[240px] h-[250px] rounded-t-md rounded-b-none object-cover cursor-pointer z-0"
             src={images[0]}
             alt="imageCard"
