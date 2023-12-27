8c47a1154f563a02d0f26781454fc3f85c784ee5
Mauricio Monzon <104470486+Majomon@users.noreply.github.com>
2023-12-27 13:03:23 -0300

    Page home

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,17 +1,17 @@
+import MainHome from "@/components/Home/MainHome";
 
 async function getAllProducts() {
   const response = await fetch(`https://teza-shoes-api.vercel.app/products`);
-  const data = await response.json()
+  const data = await response.json();
   return data;
 }
 
 async function Home() {
   const product = await getAllProducts();
-  console.log(product);
 
   return (
     <>
-      <h2>Main</h2>
+      <MainHome product={product} />
     </>
   );
 }
