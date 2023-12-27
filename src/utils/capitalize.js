b93770fee4a3e69d2c80a69203fe01e6344f94f6
Mauricio Monzon <104470486+Majomon@users.noreply.github.com>
2023-12-27 11:22:25 -0300

    Actualizacion

---
+++ src/utils/capitalize.js
@@ -0,0 +1,4 @@
+export const capitalize = (params) => {
+    return params.charAt(0).toUpperCase() + params.slice(1).toLowerCase();
+  };
+  
---
+++ src/utils/iconsContact.js
@@ -0,0 +1,74 @@
+export const iconFacebook = (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+    >
+      <g fill="none">
+        <path d="M0 0h24v24H0z" />
+        <path
+          fill="#ffffff"
+          d="M18 2a1 1 0 0 1 .993.883L19 3v4a1 1 0 0 1-.883.993L18 8h-3v1h3a1 1 0 0 1 .991 1.131l-.02.112l-1 4a1 1 0 0 1-.858.75L17 15h-2v6a1 1 0 0 1-.883.993L14 22h-4a1 1 0 0 1-.993-.883L9 21v-6H7a1 1 0 0 1-.993-.883L6 14v-4a1 1 0 0 1 .883-.993L7 9h2V8a6 6 0 0 1 5.775-5.996L15 2h3z"
+        />
+      </g>
+    </svg>
+  );
+  
+  
+  export const iconInstagram = (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+    >
+      <g fill="none" stroke="#ffffff">
+        <path d="M4 8a4 4 0 0 1 4-4h8a4 4 0 0 1 4 4v8a4 4 0 0 1-4 4H8a4 4 0 0 1-4-4z" />
+        <path d="M9 12a3 3 0 1 0 6 0a3 3 0 1 0-6 0m7.5-4.5v.01" />
+      </g>
+    </svg>
+  );
+  
+  export const iconWhatApp = (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+    >
+      <g fill="none" stroke="#ffffff">
+        <path d="m3 21l1.65-3.8a9 9 0 1 1 3.4 2.9L3 21" />
+        <path d="M9 10a.5.5 0 0 0 1 0V9a.5.5 0 0 0-1 0v1a5 5 0 0 0 5 5h1a.5.5 0 0 0 0-1h-1a.5.5 0 0 0 0 1" />
+      </g>
+    </svg>
+  );
+  
+  export const iconEmail = (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+    >
+      <g fill="none" stroke="#ffffff">
+        <path d="M3 7a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V7z" />
+        <path d="m3 7l9 6l9-6" />
+      </g>
+    </svg>
+  );
+  
+  export const iconLocation = (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+    >
+      <g fill="none" stroke="#ffffff">
+        <path d="M3 12a9 9 0 1 0 18 0a9 9 0 1 0-18 0" />
+        <path d="m12 17l-1-4l-4-1l9-4z" />
+      </g>
+    </svg>
+  );
+  
