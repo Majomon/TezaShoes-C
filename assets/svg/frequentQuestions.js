4bb970b6755ce4d76444a54d79f117744d268b82
Mauricio Monzon <104470486+Majomon@users.noreply.github.com>
2023-12-27 12:48:40 -0300

    Agregado assets

---
+++ assets/svg/frequentQuestions.js
@@ -0,0 +1,17 @@
+export const IconArrowRight = () => {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            viewBox="0 0 24 24"
+        >
+            <path
+                fill="none"
+                stroke="#AE9667"
+                strokeWidth="2"
+                d="M5 12h14m-4 4l4-4m-4-4l4 4"
+            />
+        </svg>
+    )
+}
