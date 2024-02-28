10ea5e8145ae6783750afde520f85a69bc7e6722
Mauri <mauri.monzon91@gmail.com>
2024-02-28 16:53:20 -0300

    act

--- MIRARALTERMINAR/search/page.jsx
+++ MIRARALTERMINAR/search/page.jsx
@@ -4,11 +4,14 @@ export const metadata = {
     title: "TezaShoes - Search",
     description: "Search en la web de TezaShoes",
   };
+
+  
 async function getAllProducts() {
     const response = await fetch(`${process.env.URL_BASE_DEV}/products`);
     const data = await response.json();
     return data;
 }
+
 
 export default async function (){
     const product = await getAllProducts();
