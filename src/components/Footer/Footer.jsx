a5f754c9613c0fc41f6336b1ad234ccfbe6a737a
Mauri <mauri.monzon91@gmail.com>
2024-02-28 09:01:06 -0300

    Agregado footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,111 +1,146 @@
 "use client";
-
-import Link from "next/link";
+import Cookies from "js-cookie";
 import Image from "next/image";
-import LogoTeza from "../../app/LogoTeza.png";
-import SocialMediaIcons from "../SocialMediaIcons/SocialMediaIcons";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { IconMail, IconMap, IconPhone } from "../../../assets/contact/contact";
+import LogoTeza from "../../../assets/image/logoTeza.png";
 import {
-  iconInstagram,
-  iconFacebook,
-  iconWhatApp,
-  iconEmail,
-  iconLocation,
-} from "@/utils/iconsContact";
+  IconFooterFacebook,
+  IconFooterInstagram,
+} from "../../../assets/socialMedia/SocialMedia";
 
 const optionsRedes = [
   {
     name: "Facebook",
     action: "url",
     to: "https://www.facebook.com/",
-    icon: iconFacebook,
+    icon: (
+      <IconFooterFacebook className="w-[24px] h-[24px] rounded-[100px] bg-red-500" />
+    ),
   },
   {
     name: "Instagram",
     action: "url",
     to: "https://www.instagram.com/",
-    icon: iconInstagram,
+    icon: <IconFooterInstagram className="w-[24px] h-[24px] rounded-[100px]" />,
   },
 ];
 
-const optionsContact = [
+const linkPage = [
   {
-    name: "1159728345",
-    action: "url",
-    to: "https://wa.me/+5491159728345",
-    icon: iconWhatApp,
+    name: "INICIO",
+    to: "/",
   },
   {
-    name: "Belgrano 170 - San Isidro",
-    action: "url",
-    to: "https://www.google.com/maps/place/Belgrano+170,+San+Isidro",
-    icon: iconLocation,
+    name: "PREGUNTAS FRECUENTES",
+    to: "/frequentQuestions",
   },
-  { name: "infotezashoes@gmail.com", action: "email", icon: iconEmail },
+  {
+    name: "CONTACTOS",
+    to: "/contact",
+  },
+  {
+    name: "INICIAR SECION",
+    to: "/login",
+  },
 ];
+
+const optionContac = [
+  {
+    text: "1159728345",
+    icon: <IconPhone className="w-[18px] h-[18px]" />,
+    url: "https://wa.me/+5491159728345",
+  },
+  {
+    text: "infotezashoes@gmail.com",
+    icon: <IconMail className="w-[18px] h-[18px]" />,
+    url: "#",
+  },
+  {
+    text: "Belgrano 170 - San Isidro",
+    icon: <IconMap className="w-[18px] h-[18px]" />,
+    url: "https://www.google.com/maps/place/Belgrano+170,+San+Isidro",
+  },
+];
+
 function Footer() {
+  const startTime = Cookies.get("timePurchase");
+  const pathname = usePathname();
+
+  if (pathname.startsWith("/purchase")) {
+    return null;
+  }
+  
+  if(startTime) return null
   return (
-    <footer className="w-9/12 h-full mx-auto mt-16 mb-6">
-      <div className="w-full flex justify-between">
-        {/* Redes */}
-        <div>
-          <Image width={90} src={LogoTeza} alt="Logo Teza Shoes" />
-          <p className="text-xl pt-3"> Redes Sociales </p>
-          <div className="flex gap-6 py-2">
-            {optionsRedes.map((elem, index) => (
-              <SocialMediaIcons
+    <footer className="w-full h-fit py-3 md:py-0 md:h-[250px] text-colorWhite-100 bg-gradient-to-r from-black via-zinc-800 to-zinc-600 from-65% flex flex-col items-center justify-center gap-y-[30px]">
+      <div className="w-11/12 mx-auto justify-center items-center md:gap-[50px] flex flex-col md:flex-row h-fit gap-y-5">
+        <section className="w-[111px] h-[75px] flex-col justify-between items-center inline-flex">
+          <Image src={LogoTeza} className="w-[111px] h-12" alt="imgFooter" />
+          <section className="flex gap-6 py-2">
+            {optionsRedes.map((elem, index) => {
+              const { name, action, to, icon } = elem;
+              return (
+                <Link key={index} href={to} name={name} target="_blank">
+                  {icon}
+                </Link>
+              );
+            })}
+          </section>
+        </section>
+        <div className="w-[80px] h-[0px] rotate-90 border border-amber-500 hidden md:block"></div>
+        <section className="w-fit md:w-fit h-full flex flex-row flex-wrap md:flex-col md:items-start gap-3 items-center justify-center">
+          {linkPage.map((item, index) => {
+            const { name, to } = item;
+            return (
+              <Link key={index} href={to}>
+                <p className="text-[0.9rem] font-light hover:text-colorGray-100 md:text-left text-center">
+                  {name}
+                </p>
+              </Link>
+            );
+          })}
+        </section>
+        <div className="w-[80px] h-[0px] rotate-90 border border-amber-500 hidden md:block"></div>
+        <section className="w-fit h-full flex flex-col sm:flex-row items-center justify-center md:flex-col md:items-start gap-2.5">
+          {optionContac.map((item, index) => {
+            const { text, icon, url } = item;
+            return (
+              <Link
                 key={index}
-                action={elem.action}
-                to={elem.to}
-                icon={elem.icon}
-              />
-            ))}
-          </div>
-        </div>
-        {/* Menú */}
-        <div className="flex flex-col gap-y-2">
-          <h2 className="text-xl font-bold">Menu</h2>
-          <p className="text-sm hover:text-gray-950">
-            <Link href="/">Inicio</Link>
-          </p>
-          <p className="text-sm">
-            <Link href="/contact">Contactos</Link>
-          </p>
-          <p className="text-sm">
-            <Link href="/frequentQuestions">Preguntas frecuentes</Link>
-          </p>
-        </div>
-        {/* Contactanos */}
-        <div>
-          <h2 className="text-xl font-bold">Contactanos</h2>
-          <div className="flex flex-col gap-y-2">
-            {optionsContact.map((elem, index) => (
-              <SocialMediaIcons
-                key={index}
-                action={elem.action}
-                to={elem.to}
-                icon={elem.icon}
-                name={elem.name}
-              />
-            ))}
-          </div>
-        </div>
-        {/*   <div>
-          <p className="text-xl">
-            <Link href="/">Ayuda</Link>
-          </p>
-          <p className="text-sm">
-            <Link href="/">Como comprar</Link>
-          </p>
-          <p className="text-sm">
-            <Link href="/">Medios de pago</Link>
-          </p>
-          <p className="text-sm">
-            <Link href="/">Envios</Link>
-          </p>
-          <p className="text-sm">
-            <Link href="/">Política de devolución</Link>
-          </p>
-        </div> */}
+                className="flex flex-row justify-start gap-[3px] "
+                href={url}
+                target="_blank"
+              >
+                {icon}
+                <p className="text-[0.9rem] font-light hover:text-colorGray-100">
+                  {text}
+                </p>
+              </Link>
+            );
+          })}
+        </section>
+      </div>
+      <div className="w-11-12 text-center flex flex-col sm:flex-row justify-center gap-2">
+        <span className="text-white text-[1rem] font-light font-['Martel']">
+          ©2024 todos lo derechos reservados{" "}
+        </span>
+        <section className="flex flex-row gap-x-2 justify-center items-center">
+          <span className="text-white text-[1rem] font-bold font-['Martel']">
+            TEZA
+          </span>
+          <span className="text-white text-[1rem] font-light font-['Martel']">
+          </span>
+          <span className="text-white text-[1rem] font-bold font-['Martel']">
+            -
+          </span>
+          <span className="text-white text-[1rem] font-light font-['Martel']">
+          </span>
+          <span className="text-amber-500 text-[1rem] font-bold font-['Martel']">
+            MONGAR
+          </span>
+        </section>
       </div>
     </footer>
   );
