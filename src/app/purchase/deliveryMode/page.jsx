5c63e1ae46ed1aded7a1a41e4f063574dd33d671
Mauri <mauri.monzon91@gmail.com>
2024-02-29 13:06:48 -0300

    Derecho

--- src/app/purchase/deliveryMode/page.jsx
+++ src/app/purchase/deliveryMode/page.jsx
@@ -5,8 +5,8 @@ import MainRightDelivery from "@/components/Purchase/Delivery/MainRightDelivery"
 export default function DeliveryMode() {
   return (
     <div className="w-full min-h-screen mx-auto flex flex-col items-center gap-5 justify-between my-4 lg:items-start lg:flex-row ">
-      <MainLeftDelivery />
-{/*       <MainRightDelivery /> */}
+     {/*  <MainLeftDelivery /> */}
+      <MainRightDelivery />
     </div>
   );
 }
