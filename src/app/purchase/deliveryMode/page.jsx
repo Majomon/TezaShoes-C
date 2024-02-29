4b1fb669a2a336ab59dcc7223457f428229a935a
Mauri <mauri.monzon91@gmail.com>
2024-02-29 12:53:44 -0300

    a

--- src/app/purchase/deliveryMode/page.jsx
+++ src/app/purchase/deliveryMode/page.jsx
@@ -6,7 +6,7 @@ export default function DeliveryMode() {
   return (
     <div className="w-full min-h-screen mx-auto flex flex-col items-center gap-5 justify-between my-4 lg:items-start lg:flex-row ">
       <MainLeftDelivery />
-      <MainRightDelivery />
+     {/*  <MainRightDelivery /> */}
     </div>
   );
 }
