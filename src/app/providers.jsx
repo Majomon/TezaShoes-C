1ad1d4a8e7e770109a3274a1f95bd1bb67a4579a
Mauri <mauri.monzon91@gmail.com>
2024-02-29 09:21:43 -0300

    Axios

--- src/app/providers.jsx
+++ src/app/providers.jsx
@@ -3,7 +3,11 @@ import { NextUIProvider } from "@nextui-org/react";
 import axios from "axios";
 import { Provider as JotaiProvider } from "jotai";
 
-axios.defaults.baseURL = "http://localhost:8080";
+/* axios.defaults.baseURL = "http://localhost:8080"; */
+axios.defaults.baseURL = "https://teza-shoes-api.vercel.app";
+
+
+https://teza-shoes-api.vercel.app/
 
 export function Providers({ children }) {
 
