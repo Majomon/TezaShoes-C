4e1916f6a3e769a9dc5a600f94944d3528b77596
Mauri <mauri.monzon91@gmail.com>
2024-02-28 21:14:15 -0300

    asdasd

--- src/app/account/page.jsx
+++ src/app/account/page.jsx
@@ -58,7 +58,37 @@ function Edit() {
     setHasChanges(true);
   };
 
-  return <>Hola</>;
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (hasChanges) {
+      const userId = JSON.parse(localStorage.getItem("userId"));
+      const requestData = {
+        ...dataEditForm,
+        address: dataShipping.address,
+      };
+      await fetchPutUserId(userId.id, requestData);
+
+      setHasChanges(false);
+    }
+  };
+
+  useEffect(() => {
+    setDataEditForm(userData);
+    setHasChanges(false);
+  }, [userData]);
+
+  return (
+    <>
+      {!userId && (
+        <Card className=" w-11/12 mx-auto h-[80vh] p-2 mt-4" radius="lg">
+          <Skeleton className="rounded-lg">
+            <div className="w-full h-[80vh] rounded-sm bg-default-300"></div>
+          </Skeleton>
+        </Card>
+      )}
+    </>
+  );
 }
 
 export default Edit;
