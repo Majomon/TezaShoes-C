c27320bbf9610496a45554e426836fe0c822558b
Mauri <mauri.monzon91@gmail.com>
2024-03-02 10:59:21 -0300

    SearchTable

--- src/app/dashboard/clients/list/page.jsx
+++ src/app/dashboard/clients/list/page.jsx
@@ -1,11 +1,30 @@
+"use client";
+
+import SearchTableClient from "@/components/Dashboard/SearchTable/SearchTableClient";
 import MainClient from "@/components/MainClient/MainClient";
+import { useStoreUsers } from "@/zustand/store";
+import { useState } from "react";
 
 function ListClients() {
+  const { users, setUsers, fetchAllUsers } = useStoreUsers();
+  const [stateList, setStateList] = useState([]);
+
   return (
     <div className="w-full p-6">
       <div className=" w-full max-w-[960px] mx-auto flex flex-col gap-y-2">
         <h1 className=" text-base font-bold">Clientes</h1>
-        <MainClient />
+        <SearchTableClient
+          allList={users}
+          stateList={stateList}
+          setStateList={setStateList}
+        />
+        <MainClient
+          stateList={stateList}
+          setStateList={setStateList}
+          users={users}
+          setUsers={setUsers}
+          fetchAllUsers={fetchAllUsers}
+        />
       </div>
     </div>
   );
---
+++ src/components/Dashboard/SearchTable/SearchTable.jsx
@@ -0,0 +1,50 @@
+'use client'
+import React, { useEffect, useState } from "react";
+
+export default function SearchTable({ allList, stateList, setStateList }) {
+  const [valueSearch, setValueSearch] = useState(null);
+  
+  const handleChangeInput = (e) => {
+    const { value } = e.target;
+    setValueSearch(value);
+    filterOrders();
+
+    if (!value) {
+      setStateList(allList);
+    }
+  };
+
+  useEffect(() => {
+    setStateList(allList);
+  }, []);
+
+  const filterOrders = () => {
+    let newAllOrders = allList.filter((order) => {
+      if (
+        order.dataPurchase.name
+          .toLowerCase()
+          .includes(
+            valueSearch?.toLowerCase()
+          ) /* || order?.totalCart?.toLowerCase().include(valueSearch?.toLowerCase()) */
+      ) {
+        return order;
+      }
+    });
+    setStateList(newAllOrders);
+  };
+
+  /* console.log() */
+
+  return (
+    <div className="mx-auto w-full max-w-[960px] py-2">
+      <input
+        type="text"
+        className="border-1 border-colorGray-100 rounded-lg w-full p-1"
+        placeholder="Buscar por Nombre"
+        value={valueSearch}
+        onChange={handleChangeInput}
+      />
+      {/* stateOrder.length === 0 && <p>no se encontro la busqueda</p> */}
+    </div>
+  );
+}
