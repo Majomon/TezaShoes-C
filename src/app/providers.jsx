578a7d5a7104c457d3064198f07ad2065124202e
Mauri <mauri.monzon91@gmail.com>
2024-03-04 18:40:57 -0300

    asd

--- src/app/providers.jsx
+++ src/app/providers.jsx
@@ -3,10 +3,9 @@ import { NextUIProvider } from "@nextui-org/react";
 import axios from "axios";
 import { Provider as JotaiProvider } from "jotai";
 
-
-/* axios.defaults.baseURL = "https://teza-shoes-api.vercel.app"; */
 /* axios.defaults.baseURL = "http://localhost:8080"; */
-axios.defaults.baseURL = "https://tezashoes-api-2.onrender.com";
+axios.defaults.baseURL = "https://teza-shoes-api.vercel.app";
+
 //teza-shoes-api.vercel.app/
 
 export function Providers({ children }) {
